14637b1a603c385fa2559b8fae2a3bf000ca54ef
Nigam Mehta <fornigam@gmail.com>
2022-01-07 15:04:04 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-//const morgan = require('morgan');
+const morgan = require('morgan');
 const fs = require('fs')
 const path = require('path')
 const courses = require('./routes/courses')
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const express = require('express');
+const joi = require('joi');
 
 const app = express();
-//const result = dotenv.config()
+const result = dotenv.config()
 
 //Middleware declaration
 app.use(express.json());
@@ -24,19 +25,26 @@ mongoose.connect('mongodb://localhost:27017/playground')
     .catch(()=>console.log('Error while database connection'))
 
 const curseScheme= new mongoose.Schema({
-    name : String,
+    name : {type : String, required :true },
     author : String,
     tags : [String],
     date: {type : Date, default: Date.now},
-    isPublished : Boolean
+    isPublished : {type: Boolean, required: true },
+    price : {type : Number, require : function(){
+        return this.isPublished;
+    },
+    nin : 1,
+    max : 200
+}
 });
 
 const courseModel = mongoose.model('courses',curseScheme);
 const course =  new courseModel ({
-    name : "NodeJs",
+    name : "Angular",
     author : "Nigam",
     tags : ['node','java'],
-    isPublished : true
+    isPublished : true,
+    price : 10
 })
 
 const dataInsert = async()=>{     
@@ -55,9 +63,49 @@ const dataAllFetch = async()=>{
     console.log(`Data from database is: ${resultdata}`);
 };
 
+//Find course data base on id and update the data
+const traditionaMethodUpdateCourse = async(id)=>{
+    //console.log('calling update'+ id);return;
+    const courseData = await courseModel.findById(id)
+    if(!courseData){
+        console.log('Course is not available');
+        return;
+    }
+    courseData.name = 'Angular 5.21'
+    courseData.author = 'Nigam Mehta1'
+    courseData.tags = (['PHP3', 'Wordpress3'])
+    courseData.save()
+    //console.log(courseData);
+};
+
+
+//Find course data base on id and update the data
+const deleteCourse = async(id)=>{
+    //console.log('calling update'+ id);return;
+    const courseData = await courseModel.deleteOne({_id : id})  
+    console.log("Delete Course->",courseData);
+};
+
+//Using updateOne method
+const updateCourseValue = async(id) => {
+    console.log("Updated calling");
+    const updatedata = await courseModel.updateOne({_id: id}, {
+        $set : {
+            name: 'Drupal',
+            author: 'Np Mehta'
+        }, 
+    })
+    console.log("Update docs",updatedata);
+}
+
+
 (async ()=> {  
     //await dataInsert();
-   await dataAllFetch();
+   //await dataAllFetch();
+   //traditionaMethodUpdateCourse("61d6c589279eec6259f0503d")
+   //updateCourseValue("61d6c589279eec6259f0503d");
+   deleteCourse("61d6c589279eec6259f0503d")
+   
 })();
 
 const port = process.env.port || 3000;
