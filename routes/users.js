5f15869428a50ea9989975b848ef819ef5868199
Nigam Mehta <fornigam@gmail.com>
2022-02-09 11:06:57 +0530

    Add files via upload

--- routes/users.js
+++ routes/users.js
@@ -1,29 +1,34 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const { contentSecurityPolicy } = require('helmet');
-const users = require('./../model/users')
+const users = require('./../model/users');
+const middleware = require('./../middleware/auth');
+
 const router = express.Router();
+const jwt = require('jsonwebtoken');
 
 
-router.get('/', async (req, res)  =>{
+router.get('/', middleware.auth, async (req, res)  =>{
     //res.send('calling');
+    console.log('calling');
     const userdata = await users.dataAllFetch();
     res.send(userdata);
     //console.log('Router is calling',JSON.stringify(userdata));
 })
 
 router.post('/add', async(req, res) =>{
+    const { error } = users.validateUser({fname : req.body.fname, email : req.body.email, password :req.body.password});
+    if(error) return res.status(404).send(error)
     
-    const userdata = await users.dataInsert(req.body);
+    const userdata = await users.dataInsert(req, res, req.body);
     console.log('Responce get',userdata);
     if(userdata){
         res.status(200).send('User is added successfully')
     } else {
-        res.status(404).send('User is not added successfully')
+        res.status(404).send('Email is already exist.User is not added successfully')
     }
 })
-
-router.post('/update', async(req, res) =>{
-    //console.log('Responce get',req.body);
+router.post('/update', async(req, res) =>{    
     const userupdatedata = await users.updateUserValue(req.body);
     if(userupdatedata){
         res.status(200).send('User is updated successfully.')
@@ -38,5 +43,15 @@ router.post('/delete', async(req, res) =>{
     } else {
         res.status(400).send('User is not deleted.')
     } 
+})
+
+router.post('/login', async(req, res) =>{
+    const loginData = await users.login(req, res, req.body); 
+    if(loginData != null ) {
+        const token = jwt.sign({_id :loginData }, process.env.jwttokenprovatekey)
+        res.header('x-auth-token',token).send(loginData)
+    }  
+    console.log("loging data->",loginData.uid); 
+   
 })
 module.exports = router;
